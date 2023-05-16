e9555452208f6fee30331cd78dfcfb605be8fa80
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-05-16 18:14:24 -0400

    Create Header feature boilerplate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,16 @@
+import logo from "../../assets/images/BrainFlix-logo.svg";
+
 export default function Header() {
-    return ()
+  return (
+    <header className="header">
+      <img src={logo} alt="BrainFlix-Logo" className="header__logo" />
+      <label htmlFor="search"></label>
+      <input type="text" name="search" id="search" />
+      <img
+        src="../images/Mohan-muruge.jpg"
+        alt="avatar-mohan-muruge"
+        className="header__avatar"
+      />
+    </header>
+  );
 }
