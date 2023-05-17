65e8cf6dfad46e42d4b670b6a26e83a0dbe1590d
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-05-17 07:23:21 -0400

    Complete header skeleton and Declare variables and mixins

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,36 @@
 import logo from "../../assets/images/BrainFlix-logo.svg";
+import Mohan from "../../assets/images/Mohan-muruge.jpg";
+import upload from "../../assets/images/upload.svg";
 
 export default function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="BrainFlix-Logo" className="header__logo" />
-      <label htmlFor="search"></label>
-      <input type="text" name="search" id="search" />
-      <img
-        src="../images/Mohan-muruge.jpg"
-        alt="avatar-mohan-muruge"
-        className="header__avatar"
-      />
+      <a href="/" className="logo__link">
+        <img src={logo} alt="BrainFlix-Logo" className="logo" />
+      </a>
+      <div className="header__functions">
+        <div className="search">
+          <label htmlFor="search" className="search__label"></label>
+
+          <input
+            type="text"
+            name="search"
+            id="search"
+            className="search__function"
+            placeholder="Search"
+          />
+          {/* <img
+            src={search}
+            alt="magnifying-glass-icon"
+            className="search__icon"
+          /> */}
+        </div>
+        <img src={Mohan} alt="avatar-mohan-muruge" className="header__avatar" />
+        <button className="upload__function">
+          <img src={upload} alt="upload-icon" className="upload__icon" />
+          UPLOAD
+        </button>
+      </div>
     </header>
   );
 }
