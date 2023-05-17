73aa184f458fdd04d4b7ca597e7728718b0d227b
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-05-17 17:07:05 -0400

    Complete header styling and responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,29 +5,22 @@ import upload from "../../assets/images/upload.svg";
 export default function Header() {
   return (
     <header className="header">
-      <a href="/" className="logo__link">
-        <img src={logo} alt="BrainFlix-Logo" className="logo" />
+      <a href="/">
+        <img src={logo} alt="BrainFlix-Logo" className="header__logo" />
       </a>
-      <div className="header__functions">
-        <div className="search">
-          <label htmlFor="search" className="search__label"></label>
+      <div className="header__container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="header__search"
+          placeholder="Search"
+        />
 
-          <input
-            type="text"
-            name="search"
-            id="search"
-            className="search__function"
-            placeholder="Search"
-          />
-          {/* <img
-            src={search}
-            alt="magnifying-glass-icon"
-            className="search__icon"
-          /> */}
-        </div>
         <img src={Mohan} alt="avatar-mohan-muruge" className="header__avatar" />
-        <button className="upload__function">
-          <img src={upload} alt="upload-icon" className="upload__icon" />
+
+        <button className="header__upload">
+          <img src={upload} alt="upload-icon" className="header__upload-icon" />
           UPLOAD
         </button>
       </div>
