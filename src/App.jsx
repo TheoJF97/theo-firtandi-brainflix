2445f976e834141d840d43b8d54b492df59612bb
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-05-18 16:05:53 -0400

    Push before merge to develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.scss";
 import Header from "./components/header/Header";
 import VideoPlayer from "./components/video-player/video-player";
+// import videoDetails from "./data/video-details.json";
+import videos from "./data/videos.json";
+import { useState } from "react";
 
 function App() {
+  const [selectedVideo, setSelectedVideo] = useState(videos[0]);
+
   return (
     <div className="App">
       <Header />
       <VideoPlayer />
+      {/* <VideoDetails />
+      <Comments />
+      <VideosNav /> */}
     </div>
   );
 }
--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -1,4 +1,4 @@
-import play from "../../assets/images/play.svg";
+// import play from "../../assets/images/play.svg";
 // import pause from "../../assets/images/pause.svg";
 // import volumeUp from "../../assets/images/volume_up.svg";
 // import volumeOff from "../../assets/images/volume_off.svg";
@@ -6,17 +6,17 @@ import play from "../../assets/images/play.svg";
 import videoDetails from "../../data/video-details.json";
 
 export default function VideoPlayer() {
-  console.log(videoDetails);
   let firstImgSrc = videoDetails[0].image;
-  console.log(firstImgSrc);
+  // console.log(videoDetails);
+  // console.log(firstImgSrc);
   return (
     <section className="video">
       <video controls poster={firstImgSrc} className="video__current">
-        <div className="video__controls">
+        {/* <div className="video__controls">
           <button className="video__button-play"></button>
           <button className="video__button-pause"></button>
           <button className="video__button-volume"></button>
-        </div>
+        </div> */}
       </video>
     </section>
   );
