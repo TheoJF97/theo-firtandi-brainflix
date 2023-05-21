be1d4da7bcfec912425e1a3d63271b92ce06288a
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-05-21 14:36:47 -0400

    Complete responsive styling in mobile and tablet viewport

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,7 +2,6 @@ import Mohan from "../../assets/images/Mohan-muruge.jpg";
 import commentIcon from "../../assets/images/add_comment.svg";
 
 export default function Comments({ comments }) {
-  console.log(comments.length);
   // const date = new Date(timestamp);
   // <span className="video-details__date">
   //           {date.toLocaleDateString("en-US", {
@@ -12,49 +11,56 @@ export default function Comments({ comments }) {
   //           })}
   //         </span>
 
-  //comments is an array of objects
-
-  //comments[i].name
-  //comments[i].comment
-  //comments[i].timestamp
-
   return (
     <section className="comments">
-      <span className="comments__amount">{comments.length + " Comments"}</span>
+      <p className="comments__amount">{comments.length + " Comments"}</p>
 
-      <img
-        src={Mohan}
-        alt="avatar-mohan-murge"
-        className="comments__avatar--mohan"
-      />
+      <form className="comments__form">
+        <img
+          src={Mohan}
+          alt="avatar-mohan-murge"
+          className="comments__avatar--mohan"
+        />
+        <div className="comments__cta">
+          <div className="comments__cta-box">
+            <label htmlFor="userComment" className="comments__invitation">
+              JOIN THE CONVERSATION
+            </label>
+            <textarea
+              name="userComment"
+              id="userComment"
+              className="comments__input"
+              placeholder="Add a new comment"
+            ></textarea>
+          </div>
 
-      <span className="comments__invitation">JOIN THE CONVERSATION</span>
-      <textarea
-        name=""
-        id=""
-        className="comments__input"
-        placeholder="Add a new comment"
-      ></textarea>
-
-      <button className="comments__button">
-        <img src={commentIcon} alt="" className="comments__comment-icon" />
-        COMMENT
-      </button>
-
-      {/* Create a function that ouputs comments */}
-      <img src="" alt="user-avatar" className="comments__avatar" />
-      <p className="comments__name">{comments[0].name}</p>
-      <p className="comments__timestamp">{comments[0].timestamp}</p>
-      <p className="comments__comment">{comments[0].comment}</p>
+          <button className="comments__button">
+            <img src={commentIcon} alt="" className="comments__comment-icon" />
+            COMMENT
+          </button>
+        </div>
+      </form>
 
       {comments.map((comment) => {
+        const date = new Date(comment.timestamp);
+
         return (
-          <>
-            <img src="" alt="user-avatar" className="comments__avatar" />
-            <p className="comments__name">{comment.name}</p>
-            <p className="comments__timestamp">{comment.timestamp}</p>
-            <p className="comments__comment">{comment.comment}</p>
-          </>
+          <div className="comments__container">
+            <div className="comments__avatar"></div>
+            <div className="comments__content">
+              <div className="comments__header">
+                <p className="comments__name">{comment.name}</p>
+                <span className="comments__timestamp">
+                  {date.toLocaleDateString("en-US", {
+                    month: "2-digit",
+                    day: "2-digit",
+                    year: "numeric",
+                  })}
+                </span>
+              </div>
+              <p className="comments__comment">{comment.comment}</p>
+            </div>
+          </div>
         );
       })}
     </section>
