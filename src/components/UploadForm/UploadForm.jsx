2e18d9733ee80343244cc6ac6815e9f4fbad82bb
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-05-27 11:25:56 -0400

    Complete mobile viewport styling

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 export default function UploadForm() {
   const navigate = useNavigate();
-  const alertUpload = (event) => {
+  const uploadAlert = (event) => {
     event.preventDefault();
     alert("Uploaded");
     navigate("/");
@@ -12,39 +12,56 @@ export default function UploadForm() {
 
   return (
     <section className="upload">
-      <h1 className="upload__title">Upload Video</h1>
-      <h4 className="upload__subtitle">VIDEO THUMBNAIL</h4>
+      <h1 className="upload__header">Upload Video</h1>
+      <h4 className="upload__subheader">VIDEO THUMBNAIL</h4>
       <img
         src={thumbnail}
         alt="blue-bike-in-motion"
         className="upload__thumbnail"
       />
 
-      <label htmlFor="videoTitle" className="upload__title">
-        TITLE YOUR VIDEO
-        <input type="text" className="upload__title-input" />
-      </label>
-
-      <label htmlFor="videoTitle" className="upload__title">
-        ADD A VIDEO DESCRIPTION
-        <textarea
-          name="videoTitle"
-          id="videoTitle"
-          className="upload__title-input"
-          placeholder="Add a description to your video"
-        ></textarea>
-      </label>
-      <Link to="/">
-        <button className="upload__publish" onClick={alertUpload}>
-          <img
-            src={publish}
-            alt="publish-icon"
-            className="upload__publish-icon"
+      <form className="upload__form">
+        <label htmlFor="videoTitle" className="upload__title">
+          TITLE YOUR VIDEO
+          <input
+            type="text"
+            className="upload__title-input"
+            placeholder="Add a title to your video"
           />
-          PUBLISH
-        </button>
-      </Link>
-      <button className="upload__cancel">CANCEL</button>
+        </label>
+
+        <label htmlFor="videoTitle" className="upload__description">
+          ADD A VIDEO DESCRIPTION
+          <textarea
+            name="videoTitle"
+            id="videoTitle"
+            className="upload__description-input"
+            placeholder="Add a description to your video"
+          ></textarea>
+        </label>
+
+        <div className="upload__buttons">
+          <Link to="/" className="upload__publish-link">
+            <button className="upload__publish" onClick={uploadAlert}>
+              <img
+                src={publish}
+                alt="publish-icon"
+                className="upload__publish-icon"
+              />
+              PUBLISH
+            </button>
+          </Link>
+
+          <button className="upload__cancel">
+            <img
+              src={publish}
+              alt="publish-icon"
+              className="upload__publish-icon"
+            />
+            CANCEL
+          </button>
+        </div>
+      </form>
     </section>
   );
 }
