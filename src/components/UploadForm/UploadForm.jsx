618d1c7e61467062cf13bed0493bc1cf5710eb34
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-05-27 14:11:00 -0400

    Fix upload component's divider line for desktop responsiveness

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -12,61 +12,61 @@ export default function UploadForm() {
 
   return (
     <section className="upload">
-      <h1 className="upload__header">Upload Video</h1>
+      <div className="upload__container">
+        <h1 className="upload__header">Upload Video</h1>
 
-      <form className="upload__form">
-        
-        <div className="upload__thumbnail-container">
-          <h4 className="upload__thumbnail-header">VIDEO THUMBNAIL</h4>
-          <img
-            src={thumbnail}
-            alt="blue-bike-in-motion"
-            className="upload__thumbnail"
-          />
-        </div>
-
-        <div className="upload__form-inputs">
-          <label className="upload__title">
-            TITLE YOUR VIDEO
-            <input
-              type="text"
-              className="upload__title-input"
-              placeholder="Add a title to your video"
+        <form className="upload__form">
+          <div className="upload__thumbnail-container">
+            <h4 className="upload__thumbnail-header">VIDEO THUMBNAIL</h4>
+            <img
+              src={thumbnail}
+              alt="blue-bike-in-motion"
+              className="upload__thumbnail"
             />
-          </label>
+          </div>
 
-          <label className="upload__description">
-            ADD A VIDEO DESCRIPTION
-            <textarea
-              id="videoTitle"
-              className="upload__description-input"
-              placeholder="Add a description to your video"
-            ></textarea>
-          </label>
-        </div>
+          <div className="upload__form-inputs">
+            <label className="upload__title">
+              TITLE YOUR VIDEO
+              <input
+                type="text"
+                className="upload__title-input"
+                placeholder="Add a title to your video"
+              />
+            </label>
 
-      </form>
-      <div className="upload__buttons">
-        <Link to="/" className="upload__publish-link">
-          <button className="upload__publish" onClick={uploadAlert}>
+            <label className="upload__description">
+              ADD A VIDEO DESCRIPTION
+              <textarea
+                id="videoTitle"
+                className="upload__description-input"
+                placeholder="Add a description to your video"
+              ></textarea>
+            </label>
+          </div>
+        </form>
+        <div className="upload__buttons">
+          <Link to="/" className="upload__publish-link">
+            <button className="upload__publish" onClick={uploadAlert}>
+              <img
+                src={publish}
+                alt="publish-icon"
+                className="upload__publish-icon"
+              />
+              PUBLISH
+            </button>
+          </Link>
+
+          <button className="upload__cancel">
+            {/* Add publish icon for button's centering in mobile viewport */}
             <img
               src={publish}
               alt="publish-icon"
               className="upload__publish-icon"
             />
-            PUBLISH
+            CANCEL
           </button>
-        </Link>
-
-        <button className="upload__cancel">
-          {/* Add publish icon for button's centering in mobile viewport */}
-          <img
-            src={publish}
-            alt="publish-icon"
-            className="upload__publish-icon"
-          />
-          CANCEL
-        </button>
+        </div>
       </div>
     </section>
   );
